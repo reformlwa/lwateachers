0db36a5f7ca6615369578fa9eec6457627e530db
dataonlwa <reformlwa@protonmail.com>
2020-07-29 23:52:07 -0400

    Updates

---
+++ precache-manifest.e1105670be434ba89a95990a0f8661a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f6fcb7da679808d9e0b5f8bdcf5998",
+    "url": "/lwateachers/index.html"
+  },
+  {
+    "revision": "571875b549750d904beb",
+    "url": "/lwateachers/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "554fd5a430a4ae70bfb2",
+    "url": "/lwateachers/static/css/main.38c5874a.chunk.css"
+  },
+  {
+    "revision": "571875b549750d904beb",
+    "url": "/lwateachers/static/js/2.909d5b7c.chunk.js"
+  },
+  {
+    "revision": "709c6f68f2f487eceeab20e22eecfad5",
+    "url": "/lwateachers/static/js/2.909d5b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554fd5a430a4ae70bfb2",
+    "url": "/lwateachers/static/js/main.42f4230d.chunk.js"
+  },
+  {
+    "revision": "95e10670eae9f1db506c",
+    "url": "/lwateachers/static/js/runtime-main.2625ddff.js"
+  }
+]);
